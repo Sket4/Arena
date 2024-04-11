4fb8addd729f93e6b37dfbce7de83083857b8f08
Dinar <lespaul@live.ru>
2024-04-12 02:34:07 +0300

    arena files 3

---
+++ Assets/Plugins/Managed/NLog/NLogConfig.cs
@@ -0,0 +1,108 @@
+﻿// using System;
+// using UnityEngine;
+// using NLog;
+// using NLog.Config;
+// using NLog.Targets;
+//
+// namespace TzarGames
+// {
+//     public static class NLogConfig
+//     {
+//         private static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
+//         
+//         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+//         static private void Configure()
+//         {
+//             string filename = null;
+//             
+//             var args = System.Environment.GetCommandLineArgs();
+//             for (int i = 0; i < args.Length; i++)
+//             {
+//                 string arg = args[i].ToLower();
+//
+//                 if (arg== "-logfile")
+//                 {
+//                     if (i >= args.Length - 1)
+//                     {
+//                         Debug.LogError("no argument for -logfile command");
+//                         return;
+//                     }
+//
+//                     filename = args[i + 1];
+//                 }
+//             }
+//
+//             var path = System.IO.Path.Combine(Application.streamingAssetsPath, "NLog/NLog.config");
+//             Debug.LogFormat("Загрузка конфигурации NLog из файла {0}", path);
+//             var config = new XmlLoggingConfiguration(path);
+//             LogManager.Configuration = config;
+//             
+//             
+//             if (string.IsNullOrEmpty(filename) == false)
+//             {
+//                 var target = (FileTarget)LogManager.Configuration.FindTargetByName("logfile");
+//                 target.FileName = $"${{logDirectory}}/{filename}.txt";
+//                 
+//                 Debug.Log("Log file name changed to : " + filename);
+//             }
+//
+//             Application.quitting += onAppQuit;
+//             
+// #if UNITY_EDITOR
+//             UnityEditor.EditorApplication.playModeStateChanged += EditorApplication_PlayModeStateChanged;
+//
+//             var unityTarget = new UnityConsoleLogTarget();
+//             unityTarget.Name = "Unity";
+//             LogManager.Configuration.AddTarget("Unity", unityTarget);
+//
+//             unityTarget.Layout = @"${date:format=HH\:mm\:ss} ${message} ${exception} ${event-context:item=MyValue}";
+//             var rule1 = new LoggingRule("*", LogLevel.Debug, unityTarget);
+//             LogManager.Configuration.LoggingRules.Add(rule1);
+//
+// #else
+//             Application.logMessageReceived += (condition, trace, type) =>
+//             {
+//                switch (type)
+//                {
+//                    case LogType.Error:
+//                        log.Error(condition);
+//                        break;
+//                    case LogType.Assert:
+//                        log.Error(condition);
+//                        break;
+//                    case LogType.Warning:
+//                        log.Warn(condition);
+//                        break;
+//                    case LogType.Log:
+//                        log.Info(condition);
+//                        break;
+//                    case LogType.Exception:
+//                        log.Error("{0} trace: {1}", condition, trace);
+//                        break;
+//                    default:
+//                        throw new ArgumentOutOfRangeException(nameof(type), type, null);
+//                }
+//             };
+// #endif
+//
+//             LogManager.ReconfigExistingLoggers();
+//         }
+//
+// #if UNITY_EDITOR
+//         static void EditorApplication_PlayModeStateChanged(UnityEditor.PlayModeStateChange obj)
+//         {
+//             if(obj == UnityEditor.PlayModeStateChange.ExitingPlayMode)
+//             {
+//                 LogManager.Flush();
+//                 LogManager.Shutdown();
+//             }
+//         }
+// #endif
+//
+//
+//         static void onAppQuit()
+//         {
+//             LogManager.Shutdown();
+//         }
+//     }
+// }
---
+++ Assets/Plugins/Managed/NLog/UnityConsoleLogTarget.cs
@@ -0,0 +1,36 @@
+﻿
+using NLog;
+using NLog.Targets;
+
+[Target("Unity")]
+public class UnityConsoleLogTarget : TargetWithLayout
+{
+    protected override void Write(LogEventInfo logEvent)
+    {
+        var message = Layout.Render(logEvent);
+
+        if (logEvent.Level == LogLevel.Info)
+        {
+            UnityEngine.Debug.Log(message);
+        }
+        else if(logEvent.Level == LogLevel.Error)
+        {
+            if (logEvent.Exception != null)
+            {
+                UnityEngine.Debug.LogException(logEvent.Exception);
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(message);
+            }
+        }
+        else if(logEvent.Level == LogLevel.Warn)
+        {
+            UnityEngine.Debug.LogWarning(message);
+        }
+        else
+        {
+            UnityEngine.Debug.Log(message);
+        }
+    }
+}
